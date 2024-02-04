edaab1215587314011f0d53a68c53f3957045991
victoria <148492863+V1ctor1aTorres@users.noreply.github.com>
2024-02-04 15:29:37 -0300

    Update qr-code.py

--- qr-code.py
+++ qr-code.py
@@ -1,8 +1,10 @@
 import pyqrcode
+#Funcionalidades para criar códigos QR em Python
 
 url = str(input('Digite a url para gerar o QrCode: \n'))
-
+#Usa a função create para criar um objeto QR Code com a URL fornecida
 qr = pyqrcode.create(url)
-
+#Usa o método png do objeto qr para gerar uma imagem do QR Code e salvá-la 
+#O parâmetro scale define o tamanho do bloco de pixels no QR Code
 qr.png('qrcode.png', scale=8)
 
